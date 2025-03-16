5bdf6ed8c6a6b04ae1858a90fed3931e880730ba
kakwa <carpentier.pf@gmail.com>
2025-03-16 18:44:48 +0100

    more logo tweaks

--- common/docs/conf.py
+++ common/docs/conf.py
@@ -118,7 +118,7 @@ html_theme = 'furo'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = './assets/pakste.svg'
+html_logo = './assets/pakste_w.svg'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
